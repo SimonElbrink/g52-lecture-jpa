5daf76c3474981e768d07968f332d0ed725d70f2
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-01-15 09:57:14 +0100

    JPA PART 1 - Lombok Example with Resource link
    https://projectlombok.org/features/

--- src/main/java/se/lexicon/lecturejpa/entity/Address.java
+++ src/main/java/se/lexicon/lecturejpa/entity/Address.java
@@ -1,70 +1,34 @@
 package se.lexicon.lecturejpa.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
-@Entity()
+
+// https://projectlombok.org/features/
+
+//@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(includeFieldNames = false)
+@EqualsAndHashCode()
+
+@Entity
 public class Address {
 
+    @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String street;
+
     private String city;
+
     @Column(nullable = false, length = 6)
     private String zipCode;
 
 
-
-    protected Address() {
-    }
-
-    public Address(Long id, String street, String city, String zipCode) {
-        this.id = id;
-        this.street = street;
-        this.city = city;
-        this.zipCode = zipCode;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "id=" + id +
-                ", street='" + street + '\'' +
-                ", city='" + city + '\'' +
-                ", zipCode='" + zipCode + '\'' +
-                '}';
-    }
 }
--- src/main/java/se/lexicon/lecturejpa/entity/Student.java
+++ src/main/java/se/lexicon/lecturejpa/entity/Student.java
@@ -1,11 +1,20 @@
 package se.lexicon.lecturejpa.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-// https://thorben-janssen.com/key-jpa-hibernate-annotations/
+// https://projectlombok.org/features/
+//@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+@ToString
 
+// https://thorben-janssen.com/key-jpa-hibernate-annotations/
 @Entity(name = "students")
 public class Student {
 
@@ -16,24 +25,25 @@ public class Student {
 
     @Column(nullable = false,length = 100)
     private String firstName;
+
     @Column(nullable = false,length = 100)
     private String lastName;
+
+    @NonNull
     @Column(nullable = false, unique = true)
     private String email;
 
     @Column(name = "is_active")
     private boolean active;
 
+    @ToString.Exclude
 //    @Column(columnDefinition = "DEFAULT NOW()")
     private LocalDateTime createDate;
 
     @OneToOne
     private Address address;
 
-    protected Student() {
-    }
-
-    public Student(String firstName, String lastName, String email) {
+    public Student(String firstName, String lastName, @NonNull String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -41,72 +51,4 @@ public class Student {
         this.createDate = LocalDateTime.now();
     }
 
-    public Student(String id, String firstName, String lastName, String email, boolean active, LocalDateTime createDate) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.active = active;
-        this.createDate = createDate;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public LocalDateTime getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(LocalDateTime createDate) {
-        this.createDate = createDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id='" + id + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", active=" + active +
-                ", createDate=" + createDate +
-                '}';
-    }
 }
